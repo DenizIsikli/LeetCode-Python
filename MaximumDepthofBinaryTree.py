3126f61f59d9fd7911cf49c7494d1b2223e4b6cd
DenizIsikli <s215818@dtu.dk>
2023-01-25 02:57:50 +0100

    Added comments

--- MaximumDepthofBinaryTree.py
+++ MaximumDepthofBinaryTree.py
@@ -9,3 +9,6 @@ class Solution:
         if not root: return 0
 
         return (1+max(self.maxDepth(root.left), self.maxDepth(root.right)))
+
+    # Firstly I check if the root (TreeNode) exists. If it does, I return the max of tree nodes between left and right
+    # and see which one has the most nodes, hereby returning the max depth
