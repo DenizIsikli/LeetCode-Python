6d282976a39f0837330d839d4018ca35ef2c5ab4
DenizIsikli <s215818@dtu.dk>
2023-01-25 22:32:25 +0100

    Minor changes

--- MajorityElement.py
+++ MajorityElement.py
@@ -13,7 +13,8 @@ class Solution:
 
     # Solving this with the two variables "count" and "res": "count" for how many times the number showed up
     # in the list, "res" for the current number we're on.
-    # I then go through the given list and checks if "count" is equal to 0 which it should be on the first iteration
+
+    # I then go through the given list and checks if "count" is equal to 0 which it should be on the first iteration,
     # so I then add the first number to res, and if the same number appears another place in the list, "count" adds 1
     # to itself - which it does each time res is assigned to num, otherwise it subtracts 1 meaning the numbers
     # weren't similar.
