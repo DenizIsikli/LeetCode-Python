db9a9614a652fcccd27fee1c28ea9e8ec83fc8b0
DenizIsikli <s215818@dtu.dk>
2023-01-25 02:57:25 +0100

    Added comments

--- HeightChecker.py
+++ HeightChecker.py
@@ -6,3 +6,6 @@ class Solution:
             if heights[i] != sortedList[i]:
                 result+=1
         return result
+
+    # Firstly I sort the given list to then iterate through the given list, and if "i" is not in the sorted list
+    # it adds one to the "result" variable, showcasing how many indices don't match and at last returns the result
