43f21b0b525dca307a45f2b61ed019e068ec0dd6
Bobby <s215818@dtu.dk>
2024-04-06 01:26:54 +0200

    ValidPerfectSquare.py

--- ValidPerfectSquare.py
+++ ValidPerfectSquare.py
@@ -14,3 +14,11 @@ class Solution:
                     right = mid - 1
                 else:
                     left = mid + 1
+
+    # If num is 1, return True
+    # Otherwise, set left to 0 and right to num/2
+    # While left is less than or equal to right, set mid to the average of left and right
+    # Set x to mid squared
+    # If x is equal to num, return mid
+    # If x is greater than num, set right to mid - 1
+    # If x is less than num, set left to mid + 1
