a41134293fce9318655d69415bb2be518456102f
Bobby <s215818@dtu.dk>
2024-04-09 23:32:59 +0200

    ValidateLeetCodeSolved.py

--- Validation/ValidateLeetCodeSolved.py
+++ Validation/ValidateLeetCodeSolved.py
@@ -3,7 +3,7 @@ import os
 
 class ValidateLeetcodeSolved:
     def __init__(self):
-        self.leetcode_dir = os.path.dirname("../LeetCode")
+        self.leetcode_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "Problemsets")
 
     def validate(self) -> int:
         count = 0
